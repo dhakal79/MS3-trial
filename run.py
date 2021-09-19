b41e7918751c4850d0604655729d29ebb41463d1
dhakal79 <n.dhakal@un-ihe.org>
2021-09-19 21:31:28 +0000

    create sale data function to get data string from users

--- run.py
+++ run.py
@@ -18,6 +18,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("ms3_trial")
 
-sales = SHEET. worksheet('sales')
-data = sales.get_all_values()
-print(data)
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    We run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+    data_str = input("Enter your data here: \n")
+    print(f"The data provided is {data_str}")
+get_sales_data()
+
+"""
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: \n")
+
+        sales_data = list(data_str.split(","))
+
+        if validate_data(sales_data):
+            break
+
+    return sales_data
+"""
